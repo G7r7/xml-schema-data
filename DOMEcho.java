8d0dc46c8bf51f7836d1645788aa2af3761537e8
Germain Rullier <grullier@free.fr>
2023-04-17 15:02:05 +0000

    Init

---
+++ DOMEcho.java
@@ -0,0 +1,35 @@
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+
+public class DOMEcho {
+
+    static final String outputEncoding = "UTF-8";
+
+    private static void usage() {
+        System.out.println("usage: DOMEcho <file-path>");
+    }
+
+    public static void main(String[] args) throws Exception {
+        String filename = null;
+    
+        if (args.length != 1) {
+            usage();
+            return;
+        } else {
+            filename = args[0];
+        }
+
+        if (filename == null) {
+            usage();
+            return;
+        }
+
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder(); 
+        Document doc = db.parse(new File(filename));
+    }
+}
